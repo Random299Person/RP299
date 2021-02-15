1daab9e3a79d2593a0dcb7f2012ca65a533ad212
Random299Person <79077618+Random299Person@users.noreply.github.com>
2021-02-15 13:24:04 -0500

    Create GuiLoader.lua

---
+++ RS/GuiLoader.lua
@@ -0,0 +1,11 @@
+local module = {}
+
+module.CreateWindow = function(codename, size, position, title)
+  print("Testing 2")
+end
+
+module.Response = function(typeOfResponse, data)
+  
+end
+
+return module
