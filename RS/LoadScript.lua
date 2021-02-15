3582f900863bf7fe05a6dbf904fa0cde0f7db6f9
Random299Person <79077618+Random299Person@users.noreply.github.com>
2021-02-15 13:25:47 -0500

    Update LoadScript.lua

--- RS/LoadScript.lua
+++ RS/LoadScript.lua
@@ -1,3 +1,4 @@
 --WIP
 print("TESTING")
-require()
+local module = require(game:HttpGet("https://raw.githubusercontent.com/Random299Person/RP299/main/RS/GuiLoader.lua", true))
+module.CreateWindow()
