1b09d3dfed57358042bd8783bd476597d0e4447f
Random299Person <79077618+Random299Person@users.noreply.github.com>
2021-02-15 14:07:13 -0500

    Update LoadScript.lua

--- RS/LoadScript.lua
+++ RS/LoadScript.lua
@@ -1,4 +1,25 @@
 --WIP
-print("TESTING")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Random299Person/RP299/main/RS/GuiLoader.lua", true))()
-GUI.CreateWindow()
+print("I am running!")
+local Main = Instance.new("ScreenGui", game.CoreGui)
+local RS_Title = Instance.new("ImageLabel", Main)
+RS_Title.Name = "RS_Title"
+RS_Title.AnchorPoint = Vector2.new(0.5, 0.5)
+RS_Title.BackgroundColor3 = Color3.new(1, 1, 1)
+RS_Title.BackgroundTransparency = 1
+RS_Title.Position = UDim2.new(0.5, 0, 1.125, 0)
+RS_Title.Size = UDim2.new(1, 0, 0.25, 0)
+RS_Title.Image = "http://www.roblox.com/asset/?id=6399111562"
+
+local GUI = {}
+
+GUI.CreateWindow = function(codename, size, position, title)
+  print("Testing 2")
+end
+
+GUI.Response = function(typeOfResponse, data)
+  
+end
+
+game.TweenService:Create(RS_Title, TweenInfo.new(1, Enum.EasingStyle.Back, Enum.EasingDirection.Out), {Position = UDim2.new(0.5, 0, 0.5, 0)}):Play()
+wait(4)
+game.TweenService:Create(RS_Title, TweenInfo.new(1, Enum.EasingStyle.Back, Enum.EasingDirection.In), {Position = UDim2.new(0.5, 0, 1.125, 0)}):Play()
