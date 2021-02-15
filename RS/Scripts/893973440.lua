3e1642cf16aff8a5d61abab38c84a7e023360598
Random299Person <79077618+Random299Person@users.noreply.github.com>
2021-02-15 15:15:54 -0500

    Create 893973440.lua

---
+++ RS/Scripts/893973440.lua
@@ -0,0 +1,4 @@
+local MainGui = Instance.new("ScreenGui", game.CoreGui)
+local Frame = Instance.new("Frame", MainGui)
+Frame.Size = UDim2.new(0.6, 0, 0.6, 0)
+Frame.Position = UDim2.new(0.2, 0, 0.2, 0)
