50950c79f42bf3092350b956508f209dfd8f6bcc
Random299Person <79077618+Random299Person@users.noreply.github.com>
2021-02-15 15:11:35 -0500

    Delete Scripts directory

--- Scripts/893973440.lua
+++
@@ -1,8 +0,0 @@
-local Main = Instance.new("ScreenGui", game.CoreGui)
-local MainFrame = Instance.new("Frame", Main)
-local Drag = Instance.new("TextButton", MainFrame)
-Drag.Text = "Game loaded: 893973440"
-Drag.TextScaled = true
-Drag.TextColor3 = Color3.new(1, 1, 1)
-Drag.BackgroundTransparency = 1
-Drag.TextStrokeTransparency = 0
